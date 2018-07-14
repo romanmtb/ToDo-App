b384b35e3b3aa0f5cd2a76618f0898032f379c7f
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-07-14 23:35:21 +0300

    add class counter

--- application/index.js
+++ application/index.js
@@ -22,3 +22,26 @@ Task.prototype.update = function (data) {
   }.bind(this));
   return this;
 };
+
+
+function TaskCounterUI(container, list) {
+
+  this.list = list;
+  this.cacheElements(container);
+  this.bindEvents();
+  this.update();
+
+}
+
+TaskCounterUI.prototype.cacheElements = function (container) {
+  this.container = document.querySelector(container);
+};
+
+TaskCounterUI.prototype.bindEvents = function () {
+  document.addEventListener(this.list.model.eventKeys.taskAdded, this.update.bind(this));
+  document.addEventListener(this.list.model.eventKeys.taskRemoved, this.update.bind(this));
+};
+
+TaskCounterUI.prototype.update = function (event) {
+  this.container.textContent = this.list.model.count;
+};
