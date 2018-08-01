cbfe8c6c02b360a13f32b4553ce27c56e9fe44a2
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-08-01 23:45:59 +0300

    add class total-ui

--- application/index.js
+++ application/index.js
@@ -30,7 +30,7 @@ Task.prototype.update = function (data) {
 function TaskCounterUI(container, list) {
 
   this.list = list;
-  this.cacheElements(container);
+  this. (container);
   this.bindEvents();
   this.update();
 
@@ -230,6 +230,51 @@ TaskListUI.prototype.renderButton = function (action, id, text) {
 
   return btn;
 };
+
+function TaskTotalUI(container, lists) {
+
+  this.lists = lists;
+  this.total = 0;
+
+  this.cacheElements(container);
+  this.bindEvents();
+  this.update();
+
+}
+
+TaskTotalUI.prototype.cacheElements = function (container) {
+
+  this.container = document.querySelector(container);
+
+};
+
+TaskTotalUI.prototype.bindEvents = function () {
+
+  this.lists.forEach(function(list){
+    document.addEventListener(list.model.eventKeys.taskAdded, this.update.bind(this));
+    document.addEventListener(list.model.eventKeys.taskRemoved, this.update.bind(this));
+  }.bind(this));
+
+};
+
+TaskTotalUI.prototype.update = function (event) {
+
+  this.total = 0;
+
+  this.lists.forEach(function(list){
+    this.total += list.model.count;
+  }.bind(this));
+
+  this.render();
+
+};
+
+TaskTotalUI.prototype.render = function () {
+
+  this.container.textContent = this.total;
+
+};
+
 
 
 var list1 = new TaskListUI('#tasks');
