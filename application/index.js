8544e8aa8ff678de772fc4be496a35f96cde958d
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-08-18 22:36:49 +0300

    fix class task

--- application/index.js
+++ application/index.js
@@ -1,277 +1,254 @@
-function Task(text) {
+class Task {
 
-  this.id = new Date().getTime().toString();
-  this.text = text;
+  constructor(text) {
+    this.id = new Date().getTime().toString();
+    this.text = text;
+  }
+
+  validate(newData) {
+      var data = newData || this;
+      if (!data.text || typeof data.text !== "string" || data.text === "") {
+        return false;
+      }
+      return true;
+  };
 
 }
 
-Task.prototype.validate = function(newData) {
-    var data = newData || this;
-    if (!data.text || typeof data.text !== "string" || data.text === "") {
-      return false;
+
+
+
+
+  class TaskList {
+
+    constructor(container) {
+      this.id = container.replace('#', '');
+      this.tasks = [];
+      this.count = 0;
+
+      this.eventKeys = {
+        taskAdded: this.id + ':task:added',
+        taskRemoved: this.id + ':task:removed'
+      };
+
+      this.events = {
+        taskAdded: new Event(this.eventKeys.taskAdded),
+        taskRemoved: new Event(this.eventKeys.taskRemoved)
+      };
+
     }
-    return true;
-};
 
-Task.prototype.update = function (data) {
-  if (!this.validate(data)) {
-    return false;
-  }
-  if (!this.validate(data)) {
-    return false;
-  }
-  Object.keys(data).forEach(function(key){
-    this[key] = data[key];
-  }.bind(this));
-  return this;
-};
+    addTask(task) {
+      if (!task.validate()) {
+        return;
+      }
+      this.tasks.push(task);
+      this.count++;
+      this.events.taskAdded.task = task;
+      this.events.taskAdded.count = this.count;
+      document.dispatchEvent(this.events.taskAdded);
+      return task;
+    };
 
 
-function TaskCounterUI(container, list) {
 
-  this.list = list;
-  this.cacheElements(container);
-  this.bindEvents();
-  this.update();
+    removeTask(task) {
+      var idx = this.tasks.indexOf(task);
+      this.tasks.splice(idx, 1);
+      this.count--;
+      this.events.taskRemoved.count = this.count;
+      document.dispatchEvent(this.events.taskRemoved);
+    };
 
-}
+    getTasks() {
+      return this.tasks;
+    };
 
-TaskCounterUI.prototype.cacheElements = function (container) {
-  this.container = document.querySelector(container);
-};
-
-TaskCounterUI.prototype.update = function (event) {
-  this.container.textContent = this.list.model.count;
-};
-
-TaskCounterUI.prototype.bindEvents = function () {
-  document.addEventListener(this.list.model.eventKeys.taskAdded, this.update.bind(this));
-  document.addEventListener(this.list.model.eventKeys.taskRemoved, this.update.bind(this));
-};
-
-function TaskList(container) {
-
-  this.id = container.replace('#', '');
-  this.tasks = [];
-  this.count = 0;
-
-  this.eventKeys = {
-    taskAdded: this.id + ':task:added',
-    taskUpdated: this.id + ':task:updated',
-    taskRemoved: this.id + ':task:removed'
-  };
-
-  this.events = {
-    taskAdded: new Event(this.eventKeys.taskAdded),
-    taskUpdated: new Event(this.eventKeys.taskUpdated),
-    taskRemoved: new Event(this.eventKeys.taskRemoved)
-  };
-
-}
-
-function TaskListUI(container) {
-
-  this.model = new TaskList(container);
-
-  this.cacheElements(container);
-  this.bindEvents();
-  this.renderList();
-
-}
-
-TaskListUI.prototype.cacheElements = function (container) {
-  this.container = document.querySelector(container);
-  this.form = this.container.querySelector(container + '-form');
-  this.form.taskText = this.form.querySelector('input');
-  this.list = this.container.querySelector(container + '-list');
-};
-
-TaskListUI.prototype.bindEvents = function () {
-  this.form.addEventListener('submit', this.addTask.bind(this));
-  document.addEventListener(this.model.eventKeys.taskAdded, this.renderList.bind(this));
-  document.addEventListener(this.model.eventKeys.taskAdded, this.formClearer.bind(this));
-  document.addEventListener(this.model.eventKeys.taskRemoved, this.renderList.bind(this));
-};
-
-TaskListUI.prototype.formClearer = function (event) {
-  this.form.taskText.value = '';
-};
-
-TaskListUI.prototype.addTask = function (event) {
-  event.preventDefault();
-  var text = this.form.querySelector('input').value;
-  var task = new Task(text);
-  this.model.addTask(task);
-};
-
-TaskListUI.prototype.editTask = function (event) {
-  var li = event.target.closest('li');
-  var input = li.querySelector('input');
-
-  li.classList.add('edit-mode');
-  input.focus();
-  input.setSelectionRange(0, input.value.length);
-
-};
-
-TaskListUI.prototype.updateTask = function (event) {
-  var li = event.target.closest('li');
-  var result = this.model.updateTask(li.dataset.id, {
-    text: event.target.value
-  });
-  if(result) {
-    li.classList.remove('edit-mode');
-    var span = li.querySelector('span');
-    span.textContent = result.text;
-  }
-};
-
-TaskListUI.prototype.updateTaskOnKeyPress = function (event) {
-  if ( event.which === 13 ) {
-    this.updateTask.call(this, event);
-  }
-};
-
-TaskListUI.prototype.removeTask = function (event) {
-  if (!event.target.dataset.id) {
-    alert('The task ID is required');
-    return;
-  }
-  var task = this.model.getTask(event.target.dataset.id);
-  var sure = confirm('Delete task "' + task.text  + '"?');
-  if (sure) {
-    this.model.removeTask(task);
-  }
-};
-
-TaskListUI.prototype.renderList = function () {
-
-  this.list.innerHTML = '';
-
-  var tasks = this.model.getTasks();
-
-  if (tasks.length < 1) {
-
-    var li = document.createElement('li');
-    li.classList.add('collection-item', 'grey', 'lighten-3');
-
-    var span = document.createElement('span');
-    span.textContent = 'There is no tasks! Take a break...';
-
-    li.appendChild(span);
-
-    this.list.appendChild(li);
-
-    return;
+    getTask(id) {
+      return this.tasks.find(function(task){
+        return task.id === id;
+      });
+    };
 
   }
 
-  tasks.forEach(function (task) {
-    this.renderTask(task);
-  }.bind(this));
 
-};
 
-TaskListUI.prototype.renderTask = function (task) {
+  class TaskListUI {
 
-  var li = document.createElement('li');
-  li.classList.add('collection-item');
-  li.dataset.id = task.id;
+    constructor(container) {
+      this.model = new TaskList(container);
 
-  var div = document.createElement('div');
-  div.classList.add('clear');
+      this.cacheElements(container);
+      this.bindEvents();
+      this.renderList();
+    }
 
-  var span = document.createElement('span');
-  span.textContent = task.text;
-  div.appendChild(span);
+    cacheElements(container) {
+      this.container = document.querySelector(container);
+      this.form = this.container.querySelector(container + '-form');
+      this.form.taskText = this.form.querySelector('input');
+      this.list = this.container.querySelector(container + '-list');
+    };
 
-  var input = document.createElement('input');
-  input.classList.add('left');
-  input.setAttribute('type', 'text');
-  input.value = task.text;
-  div.appendChild(input);
+    bindEvents() {
+      this.form.addEventListener('submit', this.addTask.bind(this));
+      document.addEventListener(this.model.eventKeys.taskAdded, this.renderList.bind(this));
+      document.addEventListener(this.model.eventKeys.taskAdded, this.formClearer.bind(this));
+      document.addEventListener(this.model.eventKeys.taskRemoved, this.renderList.bind(this));
+    };
 
-  var btnRemove = this.renderButton('removeTask', task.id , 'delete');
-  btnRemove.classList.add('secondary-content');
-  div.appendChild(btnRemove);
+    formClearer(event) {
+      this.form.taskText.value = '';
+    };
 
-  li.appendChild(div);
+    addTask(event) {
+      event.preventDefault();
+      var text = this.form.querySelector('input').value;
+      var task = new Task(text);
+      this.model.addTask(task);
+    };
 
-  this.list.appendChild(li);
 
-  div.addEventListener('click', this.editTask);
-  input.addEventListener('blur', this.updateTask.bind(this));
-  input.addEventListener('keypress', this.updateTaskOnKeyPress.bind(this));
-  btnRemove.addEventListener('click', this.removeTask.bind(this));
 
-};
+    updateTask(event) {
+      var li = event.target.closest('li');
+      var result = this.model.updateTask(li.dataset.id, {
+        text: event.target.value
+      });
+      if(result) {
+        li.classList.remove('edit-mode');
+        var span = li.querySelector('span');
+        span.textContent = result.text;
+      }
+    };
 
-TaskListUI.prototype.renderButton = function (action, id, text) {
+    updateTaskOnKeyPress(event) {
+      if ( event.which === 13 ) {
+        this.updateTask.call(this, event);
+      }
+    };
 
-  var btn = document.createElement('a');
-  btn.setAttribute('href', '#');
-  btn.classList.add('grey-text', 'right');
-  btn.dataset.action = action;
-  btn.dataset.id = id;
+    removeTask(event) {
+      if (!event.target.dataset.id) {
+        alert('The task ID is required');
+        return;
+      }
+      var task = this.model.getTask(event.target.dataset.id);
+      var sure = confirm('Delete task "' + task.text  + '"?');
+      if (sure) {
+        this.model.removeTask(task);
+      }
+    };
 
-  var btn = document.createElement('a');
-  btn.setAttribute('href', '#');
-  btn.classList.add('grey-text', 'right');
-  btn.dataset.action = action;
-  btn.dataset.id = id;
+    renderList() {
 
-  var icon = document.createElement('i');
-  icon.classList.add('material-icons');
-  icon.textContent = text;
-  icon.dataset.action = action;
-  icon.dataset.id = id;
+      this.list.innerHTML = '';
 
-  btn.appendChild(icon);
+      var tasks = this.model.getTasks();
 
-  return btn;
-};
+      if (tasks.length < 1) {
 
-function TaskTotalUI(container, lists) {
+        var li = document.createElement('li');
+        li.classList.add('collection-item', 'grey', 'lighten-3');
 
-  this.lists = lists;
-  this.total = 0;
+        var span = document.createElement('span');
+        span.textContent = 'There is no tasks! Take a break...';
 
-  this.cacheElements(container);
-  this.bindEvents();
-  this.update();
+        li.appendChild(span);
 
-}
+        this.list.appendChild(li);
 
-TaskTotalUI.prototype.cacheElements = function (container) {
+        return;
 
-  this.container = document.querySelector(container);
+      }
 
-};
+      tasks.forEach(function (task) {
+        this.renderTask(task);
+      }.bind(this));
 
-TaskTotalUI.prototype.bindEvents = function () {
+    };
 
-  this.lists.forEach(function(list){
-    document.addEventListener(list.model.eventKeys.taskAdded, this.update.bind(this));
-    document.addEventListener(list.model.eventKeys.taskRemoved, this.update.bind(this));
-  }.bind(this));
+    renderTask(task) {
 
-};
+      var li = document.createElement('li');
+      li.classList.add('collection-item');
+      li.dataset.id = task.id;
 
-TaskTotalUI.prototype.render = function () {
+      var div = document.createElement('div');
+      div.classList.add('clear');
 
-  this.container.textContent = this.total;
-  console.log( this.total );
+      var span = document.createElement('span');
+      span.textContent = task.text;
+      div.appendChild(span);
 
-};
+      var input = document.createElement('input');
+      input.classList.add('left');
+      input.setAttribute('type', 'text');
+      input.value = task.text;
+      div.appendChild(input);
 
-TaskTotalUI.prototype.update = function (event) {
-  this.total = 0;
+      var btnRemove = this.renderButton('removeTask', task.id , 'delete');
+      btnRemove.classList.add('secondary-content');
+      div.appendChild(btnRemove);
 
-  this.lists.forEach(function(list){
-    this.total += list.model.count;
-  }.bind(this));
-  this.render();
-};
+      li.appendChild(div);
+
+      this.list.appendChild(li);
+
+      div.addEventListener('click', this.editTask);
+      input.addEventListener('blur', this.updateTask.bind(this));
+      input.addEventListener('keypress', this.updateTaskOnKeyPress.bind(this));
+      btnRemove.addEventListener('click', this.removeTask.bind(this));
+
+    };
+
+    renderButton(action, id, text) {
+
+      var btn = document.createElement('a');
+      btn.setAttribute('href', '#');
+      btn.classList.add('grey-text', 'right');
+      btn.dataset.action = action;
+      btn.dataset.id = id;
+
+      var icon = document.createElement('i');
+      icon.classList.add('material-icons');
+      icon.textContent = text;
+      icon.dataset.action = action;
+      icon.dataset.id = id;
+
+      btn.appendChild(icon);
+
+      return btn;
+    };
+
+  }
+
+
+
+  class TaskCounterUI {
+
+    constructor(container, list) {
+      this.list = list;
+      this.cacheElements(container);
+      this.bindEvents();
+      this.update();
+    }
+
+    cacheElements(container) {
+      this.container = document.querySelector(container);
+    };
+
+    bindEvents() {
+      document.addEventListener(this.list.model.eventKeys.taskAdded, this.update.bind(this));
+      document.addEventListener(this.list.model.eventKeys.taskRemoved, this.update.bind(this));
+    };
+
+    update(event) {
+      this.container.textContent = this.list.model.count;
+    };
+
+  }
 
 
 
@@ -279,6 +256,4 @@ TaskTotalUI.prototype.update = function (event) {
 
 var list1 = new TaskListUI('#tasks');
 var list1Counter = new TaskCounterUI('#tasks-counter', list1);
-
-new TaskTotalUI('#total', [list1, list2]);
-console.log( [list1, list2] );
+console.log( '---', list1 );
